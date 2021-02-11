531cd724f6f252d4a8786890130261371e1db579
Vinny Uzor <vinny.uzor@outlook.com>
2021-02-11 13:58:21 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,29 @@
-# Define the variable board below.
+puts board = Array ["  ","  ","  ","  ","  ","  ","  ","  ","  "]
+row = [" "," "," "]
+
+# Position 1: Top left
+row[0]
+
+# Position 2: Top Middle
+row[1]
+
+#Position 3: Top right
+row[2]
+
+Position 4: Middle Left
+row [3]
+
+Position 5: Middle Middle
+row [4]
+
+Position 6: Middle Right
+row [5]
+
+Position 7: Bottom Left
+row [6]
+
+Position 8: Bottom Middle
+row[7]
+
+position 9: Bottom Right
+row [8]
